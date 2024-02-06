9dcfac8305f66f900f2dc1945834b564042267f3
rahul <rahulm@webelight.co.in>
2024-02-06 19:13:36 +0530

    Comment-out the external js file

--- dist/slickCarousel.js
+++ dist/slickCarousel.js
@@ -1,40 +1,40 @@
-// let carousel = document.querySelector(".responsive");
-// console.log(carousel);
-$(document).ready(function () {
-  $(".responsive").slick({
-    dots: true,
-    infinite: true,
-    autoplay: false,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 450,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ],
-  });
-});
+// // let carousel = document.querySelector(".responsive");
+// // console.log(carousel);
+// $(document).ready(function () {
+//   $(".responsive").slick({
+//     dots: true,
+//     infinite: true,
+//     autoplay: false,
+//     speed: 500,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 1,
+//           infinite: true,
+//           dots: true,
+//         },
+//       },
+//       {
+//         breakpoint: 600,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 450,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       // You can unslick at a given breakpoint now by adding:
+//       // settings: "unslick"
+//       // instead of a settings object
+//     ],
+//   });
+// });
